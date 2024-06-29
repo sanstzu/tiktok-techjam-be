078056a2d8ac47d34ab5d424ffe885b24b231edb
Clayton Fernalo <clafernalo@gmail.com>
2024-06-30 00:25:35 +0800

    add audio splitter

--- ML/video.py
+++ ML/video.py
@@ -9,7 +9,9 @@ import time
 # Constants
 download_output = os.path.join(".", "download")
 frames_output = os.path.join(".", "frames")
-interval = 5
+audio_output = os.path.join(".", "audio")
+interval = 15
+image_per_interval = 3
 
 def extract_youtube_id(url):
     regex = r'https:\/\/(?:www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]{11})|https:\/\/youtu\.be\/([a-zA-Z0-9_-]{11})'
@@ -33,14 +35,20 @@ class TestCaseGenerator:
         
         if os.path.exists(frames_output) == False:
             os.mkdir(frames_output)
+        
+        if os.path.exists(audio_output) == False:
+            os.mkdir(audio_output)
 
         output = self.__download()
-        self.__split(output)
+        # self.__split_video(output[0])
+        self.__split_audio(output[1])
 
     def __download(self):
         id = extract_youtube_id(self.url)
         file_name = f"{id}.mp4"
+        audio_file_name = f"{id}.mp3"
         output_path = os.path.join(download_output, file_name)
+        audio_output_path = os.path.join(download_output, audio_file_name)
 
         if os.path.exists(output_path):
             return output_path
@@ -54,15 +62,30 @@ class TestCaseGenerator:
 
         yt_stream.download(download_output, filename=file_name)
         print("Finished downloading after: ", time.time() - start)
-        return output_path
+
+        yt_audio_stream = yt.streams.filter(only_audio=True).first()
+        yt_audio_stream.download(download_output, filename=audio_file_name)
+        print("Finished downloading audio after: ", time.time() - start)
+
+        print(audio_file_name, file_name)
+        return [output_path, audio_output_path]
     
-    def __split(self, video_path):
+    def __split_video(self, video_path):
         output_folder = os.path.join(frames_output, extract_youtube_id(self.url))
         if os.path.exists(output_folder) == False:
             os.mkdir(output_folder)
 
         output_image = os.path.join(output_folder, "frame_%04d.jpeg")
-        cmd = ["ffmpeg", "-i", video_path, "-r", str(1/interval), output_image]
+        cmd = ["ffmpeg", "-i", video_path, "-r", str(image_per_interval/interval), output_image]
+        os.system(" ".join(cmd))
+    
+    def __split_audio(self, video_path):
+        output_folder = os.path.join(audio_output, extract_youtube_id(self.url))
+        if os.path.exists(output_folder) == False:
+            os.mkdir(output_folder)
+
+        output_audio = os.path.join(output_folder, "audio_%04d.mp3")
+        cmd = ["ffmpeg", "-i", video_path, "-f", "segment", "-segment_time", str(interval), "-q:a", "0", "-map" , "a", output_audio]
         os.system(" ".join(cmd))
 
 if __name__ == "__main__":
