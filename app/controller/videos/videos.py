8993c1fc66c4678202ad4824b286582b966712b7
Clayton Fernalo <clafernalo@gmail.com>
2024-07-06 02:04:10 +0800

    feat: apply video model

--- app/controller/videos/videos.py
+++ app/controller/videos/videos.py
@@ -2,18 +2,40 @@ from app.models.user import User
 from pydantic import BaseModel
 import datetime
 from app.models.videos import VideoModel
+from typing import List
 
-sample_video = [
-    "https://tiktok-techjam.s3.ap-southeast-2.amazonaws.com/videos/sample-video-1.mp4"
-    "https://tiktok-techjam.s3.ap-southeast-2.amazonaws.com/videos/sample-video-2.mp4"
-    "https://tiktok-techjam.s3.ap-southeast-2.amazonaws.com/videos/sample-video-3.mp4"
-]
+sample_video_1: VideoModel = VideoModel(
+    id="1",
+    username="user1",
+    caption="caption1",
+    music="Adele - Hello",
+    videoUrl="https://tiktok-techjam.s3.ap-southeast-2.amazonaws.com/videos/sample-video-1.mp4",
+    userId="user1"
+)
+
+sample_video_2: VideoModel = VideoModel(
+    id="2",
+    username="user1",
+    caption="English or Spanish",
+    music=None,
+    videoUrl="https://tiktok-techjam.s3.ap-southeast-2.amazonaws.com/videos/sample-video-2.mp4",
+    userId="user1"
+)
+
+sample_video_3: VideoModel = VideoModel(
+    id="3",
+    username="user2",
+    caption="Spelling Bee Contest Championship",
+    music=None,
+    videoUrl="https://tiktok-techjam.s3.ap-southeast-2.amazonaws.com/videos/sample-video-3.mp4",
+    userId="user2"
+)
     
 
 async def get_videos_controller(user: str):
     if user is None:
         # TODO: query all videos from the database
-        return sample_video
+        return [sample_video_1, sample_video_2, sample_video_3]
     else:
         # TODO: query all videos for a user id
-        return sample_video[0:2]
+        return [sample_video_1, sample_video_2]
