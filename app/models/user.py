ec04a1743bec2df6e13e0f919e6e462d7dfdd6ab
mattdinata <matt.dinata@gmail.com>
2024-07-06 14:29:42 +0800

    feat: add request middleware

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
+from datetime import datetime
+from typing import Optional
+import uuid
 
 class User(BaseModel):
-    email: str
-    password: str
+    id: str = Field(default_factory=uuid.uuid4)
+    name: Optional[str] = None
+    email: EmailStr
+    emailVerified: Optional[datetime] = None
+    image: Optional[str] = None
