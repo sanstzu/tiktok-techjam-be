4a548ba1094c5c424104a3477752379156d9c7ea
mattdinata <matt.dinata@gmail.com>
2024-07-07 23:02:48 +0800

    feat: add post time for videos

--- app/controller/highlights/results.py
+++ app/controller/highlights/results.py
@@ -3,6 +3,7 @@ from fastapi.responses import JSONResponse
 from app.schemas.highlights import HighlightsPostRequest
 from app.database.db import get_db
 import uuid
+import datetime
 
 db = get_db()
 
@@ -24,13 +25,14 @@ async def get_results_controller(task_id: str) -> JSONResponse:
     
 
 
-async def post_video_controller(post_request: HighlightsPostRequest, user_id: str) -> str:
+async def post_video_controller(post_request: HighlightsPostRequest, task_id: str, user_id: str) -> str:
     query = """
-    INSERT INTO video (id, "videoUrl", caption, music, "userId")
-    VALUES (:id, :video_url, :caption, :music, :user_id)
+    INSERT INTO video (id, "videoUrl", caption, music, "userId", "taskId", "postTime")
+    VALUES (:id, :video_url, :caption, :music, :user_id, :task_id, :post_time)
     """
     try:
-        await db.execute(query=query, values={"id": str(uuid.uuid4()), "video_url": post_request.video_url, "caption": post_request.caption, "music": post_request.music, "user_id": user_id})
+        current_utc_time = datetime.datetime.now(datetime.timezone.utc) 
+        await db.execute(query=query, values={"id": str(uuid.uuid4()), "video_url": post_request.video_url, "caption": post_request.caption, "music": post_request.music, "user_id": user_id, "task_id": task_id, "post_time": current_utc_time})
 
         return "Video metadata updated"
     except Exception as e:
