be910f288d22c65fb30b81f8522e23419a683290
mattdinata <matt.dinata@gmail.com>
2024-07-07 11:51:09 +0800

    chore: move fyp videos sample response to controller

--- app/controller/videos/videos.py
+++ app/controller/videos/videos.py
@@ -5,6 +5,34 @@ from app.database.db import get_db
 
 db = get_db()
 
+def get_fyp_videos_controller() -> List[VideoResponse]:
+    sample_video_1 = VideoResponse(
+        id="testid-1",
+        caption="what's cooler than a hot chocolate?",
+        music="Adele - Hello",
+        videoUrl="https://tiktok-techjam.s3.ap-southeast-2.amazonaws.com/videos/sample-video-1.mp4",
+        name="RickyGlow"
+    )
+
+    sample_video_2 = VideoResponse(
+        id="testid-2",
+        caption="English or Spanish",
+        music=None,
+        videoUrl="https://tiktok-techjam.s3.ap-southeast-2.amazonaws.com/videos/sample-video-2.mp4",
+        name="Martinez Twins"
+    )
+
+    sample_video_3 = VideoResponse(
+        id="testid-3",
+        caption="Spelling Bee Contest Championship",
+        music="original audio - Masuk Pak Eko",
+        videoUrl="https://tiktok-techjam.s3.ap-southeast-2.amazonaws.com/videos/sample-video-3.mp4",
+        name="Lius Lasahido"
+    )
+
+    return [sample_video_1, sample_video_2, sample_video_3]
+
+
 async def get_user_videos_controller(user_id: str) -> List[VideoResponse]:
     query = """
     SELECT video.id, video.caption, video.music, video."videoUrl", "user"."name"
--- app/routes/videos.py
+++ app/routes/videos.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Request
-from app.controller.videos.videos import get_user_videos_controller
+from app.controller.videos.videos import get_user_videos_controller, get_fyp_videos_controller
 from app.schemas.videos import VideoResponse
 from typing import List
 
@@ -10,32 +10,7 @@ async def video_download():
     """
     Fetch FYP videos
     """
-
-    sample_video_1 = VideoResponse(
-        id="testid-1",
-        caption="what's cooler than a hot chocolate?",
-        music="Adele - Hello",
-        videoUrl="https://tiktok-techjam.s3.ap-southeast-2.amazonaws.com/videos/sample-video-1.mp4",
-        name="RickyGlow"
-    )
-
-    sample_video_2 = VideoResponse(
-        id="testid-2",
-        caption="English or Spanish",
-        music=None,
-        videoUrl="https://tiktok-techjam.s3.ap-southeast-2.amazonaws.com/videos/sample-video-2.mp4",
-        name="Martinez Twins"
-    )
-
-    sample_video_3 = VideoResponse(
-        id="testid-3",
-        caption="Spelling Bee Contest Championship",
-        music="original audio - Masuk Pak Eko",
-        videoUrl="https://tiktok-techjam.s3.ap-southeast-2.amazonaws.com/videos/sample-video-3.mp4",
-        name="Lius Lasahido"
-    )
-
-    return [sample_video_1, sample_video_2, sample_video_3]
+    return get_fyp_videos_controller()
 
 @router.get("/", response_model=List[VideoResponse])
 async def video_download(request: Request):
